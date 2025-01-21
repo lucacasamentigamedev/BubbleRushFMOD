6d46a1add369d59a77ea9680b14996bbd9d0d39c
FabioGiannino <fabiogiannino2@gmail.com>
2025-01-21 16:59:41 +0100

    Add simple classes for bubble and pluriball

---
+++ Assets/GGJ2025/Scripts/Pluriball/PluriballMng.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PluriballMng
+{
+
+
+    /*Generate new pluribal with procedural algorithm*/
+
+    /*Win Condition*/
+}
